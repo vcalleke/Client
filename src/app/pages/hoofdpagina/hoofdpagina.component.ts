29fd6df0c6f2d634aee772e74c4a9b2d85d39f85
vcalleke <vcal3221@gmail.com>
2025-08-07 16:33:59 +0200

    Stijl gefixt

--- src/app/pages/hoofdpagina/hoofdpagina.component.ts
+++ src/app/pages/hoofdpagina/hoofdpagina.component.ts
@@ -6,10 +6,20 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [RouterModule],
   template: `
+  <header class="app-header">
     <h1>Hoofdpagina</h1>
-    <button routerLink="/bestellen">Bestellen</button>
-    <button routerLink="/overdragen">Overdragen</button>
-    <button routerLink="/vervoersgegevens">Vervoersgegevens</button>
+  </header>
+    <div class="page-container">
+      <div class="section">
+       
+        <p>Kies een actie om verder te gaan:</p>
+        <div class="button-group">
+          <button class="btn btn-primary" routerLink="/bestellen">Bestellen</button>
+          <button class="btn btn-primary" routerLink="/overdragen">Overdragen</button>
+          <button class="btn btn-primary" routerLink="/vervoersgegevens">Vervoersgegevens</button>
+        </div>
+      </div>
+    </div>
   `
 })
 export class HoofdpaginaComponent {}
